4d31e3ea87190e62d73cd38d95fb24eecba12a5a
Bruk Gurmesa <bruktekalgne@gmail.com>
2023-06-12 20:38:23 -0400

    security group

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "alb" {
 
   vpc_id             = module.blog_vpc.vpc_id 
   subnets            = module.blog_vpc.public_subnets[0]
-  security_groups    = [module.blog_gs.security_group_id]
+  security_groups    = [module.blog_sg.security_group_id]
 
   access_logs = {
     bucket = "my-alb-logs"
