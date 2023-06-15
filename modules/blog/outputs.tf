d6ba824314c3d2b218e6c8f22134e23bc72859ec
Bruk Gurmesa <bruktekalgne@gmail.com>
2023-06-15 13:34:28 -0400

    added description

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -1,7 +1,8 @@
 output "environment_url" {
-    value = module.blog_alb.lb_dns_name
+    value       = module.blog_alb.lb_dns_name
+    description = "The public url of the ALB."
 }
 
-output "security_group_id" {
-    value = module.blog_alb.security_group_id
+output "security_group_name" {
+    value = module.blog_sg.security_group_name
 }
