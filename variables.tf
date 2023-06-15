a0c0fcc95bdf4a2c8ea2adb9c328609c685072b1
Bruk Gurmesa <bruktekalgne@gmail.com>
2023-06-15 11:32:59 -0400

    variable nameing fixed

--- variables.tf
+++ variables.tf
@@ -6,12 +6,12 @@ variable "instance_type" {
 variable "ami_filter"{
   description = "Name filter and owner for AMI"
 
-  type = object({
+  type = object ({
     name  = string 
     owner = string
   })
 
-  default {
+  default = {
     name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
     owners = "979382823631"
   }
@@ -19,24 +19,24 @@ variable "ami_filter"{
 
 variable "environment" {
   description = "Development environment"
-  type = object({
+  type = object ({
     name           = string
     network_prefix = string
   })
 
-  default = ({
-  name = "dev"
-  network_prefix = "10.0"
-  })
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
 }
 
 
-varibale  "asg_min_size" {
+variable "asg_min_size" {
   description = "Minimum number of instances in ASG"
-  default = 1
+  default     = 1
 } 
 
 variable  "asg_max_size"{
   description = "Maximum number of instances in ASG"
-  default = 2
+  default     = 2
 } 
